a6aa7ac260f2d37384f894baac83c5239427eb07
Panikkos88 <52826070+Panikkos88@users.noreply.github.com>
2023-12-12 11:04:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.tooltipped');
     var instances = M.Tooltip.init(elems);
 
     var selectElems = document.querySelectorAll('select');
     var instancesSelect = M.FormSelect.init(selectElems);
-
-    // Load previously stored URLs on page load
-    loadStoredURLs();
 });
 
 function buildURL() {
     // Your existing code to build the URL
     var utmURL = generateUTMURL();
 
-    // Save the generated URL to local storage
-    saveURLToStorage(utmURL);
+    // Display the generated URL in the textarea
+    document.getElementById('utmResult').value = utmURL;
+
+    // Show the result section
+    document.getElementById('result').classList.remove('hidden');
 }
 
 function generateUTMURL() {
-    var utmURL;
+
 
     var websiteURL = document.getElementById('website').value;
     var source = document.getElementById('source').value;
@@ -46,43 +46,33 @@ function generateUTMURL() {
     return utmURL;
 }
 
-function saveURLToStorage(url) {
-    // Get existing stored URLs or initialize an empty array
-    var storedURLs = JSON.parse(localStorage.getItem('storedURLs')) || [];
-
-    // Add the new URL to the array
-    storedURLs.push(url);
-
-    // Save the updated array back to local storage
-    localStorage.setItem('storedURLs', JSON.stringify(storedURLs));
-
-    // Load the updated list of stored URLs
-    loadStoredURLs();
+function copyToClipboard() {
+    var copyText = document.getElementById('utmResult');
+    copyText.select();
+    document.execCommand('copy');
+    M.toast({ html: 'URL copied to clipboard!', classes: 'rounded' });
 }
 
-function loadStoredURLs() {
-    // Get stored URLs from local storage
-    var storedURLs = JSON.parse(localStorage.getItem('storedURLs')) || [];
+function handleSourceChange() {
+    var source = document.getElementById('source').value;
 
-    // Display the stored URLs in a separate tab or section
-    displayStoredURLs(storedURLs);
+    // Toggle visibility of custom source input based on the selected source
+    var customSourceContainer = document.getElementById('customSourceContainer');
+    if (source === 'manual') {
+        customSourceContainer.style.display = 'block';
+    } else {
+        customSourceContainer.style.display = 'none';
+    }
 }
 
-function displayStoredURLs(urls) {
-    // Display the stored URLs in a separate tab or section
-    var storedURLsContainer = document.getElementById('storedURLsContainer');
-    if (storedURLsContainer) {
-        storedURLsContainer.innerHTML = '<h4>Stored URLs:</h4>';
-        if (urls.length > 0) {
-            var ul = document.createElement('ul');
-            urls.forEach(function (url) {
-                var li = document.createElement('li');
-                li.textContent = url;
-                ul.appendChild(li);
-            });
-            storedURLsContainer.appendChild(ul);
-        } else {
-            storedURLsContainer.innerHTML += '<p>No stored URLs.</p>';
-        }
+function handleMediumChange() {
+    var medium = document.getElementById('medium').value;
+
+    // Toggle visibility of custom medium input based on the selected medium
+    var customMediumContainer = document.getElementById('customMediumContainer');
+    if (medium === 'manual') {
+        customMediumContainer.style.display = 'block';
+    } else {
+        customMediumContainer.style.display = 'none';
     }
 }
